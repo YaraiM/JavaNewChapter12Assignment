124d31d8dc1f4a7e799b60ca14f1f9731dfda817
YaraiM <hara_kiri1990@yahoo.co.jp>
2024-05-22 02:29:23 +0900

    ソースコードを作成

---
+++ src/main/java/smart/ver/Main.java
@@ -0,0 +1,9 @@
+package smart.ver;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Library library = new Library(); //Libraryクラスのインスタンス化（≒本棚に書籍を入れる作業を実行するイメージ）
+    Library.search(); //Libraryクラスのsearchメソッドを呼び出し
+  }
+}
